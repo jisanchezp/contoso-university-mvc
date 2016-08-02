715d424cb14c949b06282f0f95abefe065eff786
Juan Ignacio Sánchez <Juan Ignacio Snchez>
2016-08-02 17:37:54 -0400

    Added student controller.

--- ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
@@ -7,7 +7,7 @@ using ContosoUniversity.Models;
 
 namespace ContosoUniversity.DAL
 {
-    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    public class SchoolInitializer : DropCreateDatabaseIfModelChanges<SchoolContext>
     {
         protected override void Seed(SchoolContext context)
         {
--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -8,6 +8,8 @@ namespace ContosoUniversity.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        public int? Age { get; set; }
+        public string EmailAddress { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
